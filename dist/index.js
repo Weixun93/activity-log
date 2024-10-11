5b669dae727273f05ff32315b2103a8900170f95
Weixun93 <93048104+Weixun93@users.noreply.github.com>
2024-10-11 08:08:41 +0000

    dist build

--- dist/index.js
+++ dist/index.js
@@ -30351,7 +30351,7 @@ module.exports = {
 const github = __nccwpck_require__(2335);
 const core = __nccwpck_require__(9619);
 const eventDescriptions = __nccwpck_require__(2619);
-const { username, token, eventLimit, style, ignoreEvents } = __nccwpck_require__(9530);
+const { username, token, eventLimit, style, targetRepos, ignoreEvents } = __nccwpck_require__(9530);
 
 // Create an authenticated Octokit client
 const octokit = github.getOctokit(token);
@@ -30423,7 +30423,7 @@ async function fetchAllEvents() {
 
             // Check for API rate limit or pagination issues
             if (events.length === 0) {
-                core.warning('⚠️ No more events available.');
+                core.warning('⚠️ T5: No more events available.');
                 break; // No more events to fetch
             }
 
@@ -30454,6 +30454,7 @@ async function fetchAndFilterEvents() {
         filteredEvents = allEvents
             .filter(event => !ignoreEvents.includes(event.type))
             .filter(event => !isTriggeredByGitHubActions(event))
+            .filter(event => targetRepos.includes(event.repo.name))
             .map(event => {
                 if (event.type === 'WatchEvent') {
                     const isStarred = starredRepoNames.has(event.repo.name);
@@ -30463,13 +30464,14 @@ async function fetchAndFilterEvents() {
                 return event;
             })
             .slice(0, eventLimit);
-
-        if (filteredEvents.length < eventLimit) {
-            const additionalEvents = await fetchAllEvents();
-            allEvents = additionalEvents.concat(allEvents);
-        } else {
-            break;
-        }
+        break;
+        // if (filteredEvents.length < eventLimit) {
+        //     const additionalEvents = await fetchAllEvents();
+        //     if (additionalEvents.length === 0) break;
+        //     allEvents = additionalEvents.concat(allEvents);
+        // } else {
+        //     break;
+        // }
     }
 
     filteredEvents = filteredEvents.slice(0, eventLimit);
